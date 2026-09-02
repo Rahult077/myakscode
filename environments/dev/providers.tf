94e64f4b56cc5d80cc991a497af98e3bc13c8ba0
Rahul Tyagi <rahulmanu130@gmail.com>
2026-09-03 01:02:43 +0530

    Configure Azure remote Terraform backend

--- environments/dev/providers.tf
+++ environments/dev/providers.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "QA"
+    storage_account_name = "mystateaks123456"
+    container_name       = "tfstate"
+    key                  = "myaks-dev.tfstate"
+  }
 }
 
 provider "azurerm" {
